7bed9ca9d41437644dbbd85ab9bae6a62f37651a
tarangsachaniya <89240529+tarangsachaniya@users.noreply.github.com>
2026-05-22 14:55:14 +0530

    Refactor layout component to use PaddingWrapper

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,10 +1,10 @@
 import { NavBar } from '@/components/navbar'
 import { Footer } from '@/components/footer'
 import { CouponTicker } from '@/components/coupon-ticker'
+import { PaddingWrapper } from '@/components/padding-wrapper'
 import { getFooterContent, getNavigationCategories, getHomepageData } from '@/lib/cache/homepage'
-import { headers } from 'next/headers'
 
-export default async function PublicLayout({ children }: { children: React.ReactNode }) {
+export default async function PublicLayout({ children }: { children: import('react').ReactNode }) {
   const [footerData, categories, homepageData] = await Promise.all([
     getFooterContent(),
     getNavigationCategories(),
@@ -13,24 +13,13 @@ export default async function PublicLayout({ children }: { children: React.React
 
   const activeCoupons = homepageData.activeCoupons ?? []
 
-  const headersList = await headers()
-  const pathname = headersList.get('x-pathname') ?? ''
-
-  const isSpecialPage = /^\/(products|checkout|account|orders)(\/|$)/.test(pathname)
-
-  const paddingTop = isSpecialPage
-    ? '90px'
-    : activeCoupons.length > 0
-    ? '126px'
-    : '0px'
-
   return (
     <>
       <CouponTicker coupons={activeCoupons} />
       <NavBar categories={categories} tickerVisible={activeCoupons.length > 0} />
-      <div style={{ paddingTop }}>
+      <PaddingWrapper hasCoupons={activeCoupons.length > 0}>
         {children}
-      </div>
+      </PaddingWrapper>
       <Footer data={footerData} />
     </>
   )
