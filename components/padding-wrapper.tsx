e2e96a5ff6175cab4978bd4dab42ba75deb7cb99
tarangsachaniya <89240529+tarangsachaniya@users.noreply.github.com>
2026-05-22 14:54:44 +0530

    Add PaddingWrapper component for dynamic padding

---
+++ components/padding-wrapper.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+
+export function PaddingWrapper({ children, hasCoupons }: { children: React.ReactNode; hasCoupons: boolean }) {
+  const pathname = usePathname()
+  const isSpecialPage = /^\/(products|checkout|account|orders)(\/|$)/.test(pathname)
+
+  const paddingTop = isSpecialPage
+    ? '90px'
+    : hasCoupons
+    ? '126px'
+    : '0px'
+
+  return <div style={{ paddingTop }}>{children}</div>
+}
