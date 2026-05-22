1b4d1d98d4a9519d3e83f23ecffbbf807c8ccf19
tarangsachaniya <89240529+tarangsachaniya@users.noreply.github.com>
2026-05-22 14:47:09 +0530

    Adjust paddingTop based on pathname and activeCoupons

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -2,6 +2,7 @@ import { NavBar } from '@/components/navbar'
 import { Footer } from '@/components/footer'
 import { CouponTicker } from '@/components/coupon-ticker'
 import { getFooterContent, getNavigationCategories, getHomepageData } from '@/lib/cache/homepage'
+import { headers } from 'next/headers'
 
 export default async function PublicLayout({ children }: { children: React.ReactNode }) {
   const [footerData, categories, homepageData] = await Promise.all([
@@ -12,11 +13,22 @@ export default async function PublicLayout({ children }: { children: React.React
 
   const activeCoupons = homepageData.activeCoupons ?? []
 
+  const headersList = await headers()
+  const pathname = headersList.get('x-pathname') ?? ''
+
+  const isSpecialPage = /^\/(products|checkout|account|orders)(\/|$)/.test(pathname)
+
+  const paddingTop = isSpecialPage
+    ? '90px'
+    : activeCoupons.length > 0
+    ? '126px'
+    : '0px'
+
   return (
     <>
       <CouponTicker coupons={activeCoupons} />
       <NavBar categories={categories} tickerVisible={activeCoupons.length > 0} />
-      <div style={{ paddingTop: activeCoupons.length > 0 ? "126px" : "0px" }}>
+      <div style={{ paddingTop }}>
         {children}
       </div>
       <Footer data={footerData} />
